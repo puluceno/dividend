ffd53f843a99ad80eda02a728b22fc75a07aa2ae
pulu <puluceno@gmail.com>
2019-12-09 22:50:28 -0600

    Added SP500 to list Paralelize data gathering

--- src/main/java/com/pulu/dividend/core/Constants.java
+++ src/main/java/com/pulu/dividend/core/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
 
     public final static String REITS_FILENAME = "reits.csv";
     public final static String SP500_FILENAME = "sp500.csv";
-    public static final String DIVIDENDS_REITS_FILENAME = "dividends_REITS" + LocalDate.now() + ".csv";
-    public static final String DIVIDENDS_SP500_FILENAME = "dividends_SP500" + LocalDate.now() + ".csv";
+    public static final String DIVIDENDS_REITS_FILENAME = "dividends_REITS_" + LocalDate.now() + ".csv";
+    public static final String DIVIDENDS_SP500_FILENAME = "dividends_SP500_" + LocalDate.now() + ".csv";
 }
